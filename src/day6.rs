de8792638d680c02ed18e7b2e3e4c00abb3fdc8d
Andrew Houts <ahouts4@gmail.com>
2021-12-13 11:05:26 -0600

    day 6

---
+++ src/day6.rs
@@ -0,0 +1,49 @@
+use num_bigint::BigInt;
+use num_traits::Zero;
+use std::io::BufRead;
+
+pub fn part1<R: BufRead>(reader: R) -> u32 {
+    let line = reader.lines().next().unwrap().unwrap();
+    let mut counts = [0; 9];
+    line.split(',')
+        .map(|n| n.parse::<usize>().unwrap())
+        .for_each(|n| {
+            counts[n] += 1;
+        });
+
+    for _ in 0..80 {
+        let mut new_counts = [0; 9];
+        for i in 0..8 {
+            new_counts[i] += counts[i + 1];
+        }
+        new_counts[6] += counts[0];
+        new_counts[8] += counts[0];
+
+        std::mem::swap(&mut new_counts, &mut counts);
+    }
+
+    counts.iter().sum()
+}
+
+pub fn part2<R: BufRead>(reader: R) -> BigInt {
+    let line = reader.lines().next().unwrap().unwrap();
+    let mut counts: [BigInt; 9] = Default::default();
+    line.split(',')
+        .map(|n| n.parse::<usize>().unwrap())
+        .for_each(|n| {
+            counts[n] += 1;
+        });
+
+    for _ in 0..256 {
+        let mut new_counts: [BigInt; 9] = Default::default();
+        for i in 0..8 {
+            new_counts[i] += &counts[i + 1];
+        }
+        new_counts[6] += &counts[0];
+        new_counts[8] += &counts[0];
+
+        std::mem::swap(&mut new_counts, &mut counts);
+    }
+
+    counts.iter().sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let p = std::path::Path::new("day5.txt");
+    let p = std::path::Path::new("day6.txt");
     let f = std::fs::File::open(p)?;
-    println!("{}", day5::part2(BufReader::new(f)));
+    println!("{}", day6::part2(BufReader::new(f)));
     Ok(())
 }
