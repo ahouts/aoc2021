32e7bd030780395645843caef1de5ff9136d5a79
Andrew Houts <ahouts4@gmail.com>
2021-12-13 11:07:27 -0600

    bigint not required

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,3 @@
-use num_bigint::BigInt;
-use num_traits::Zero;
 use std::io::BufRead;
 
 pub fn part1<R: BufRead>(reader: R) -> u32 {
@@ -25,9 +23,9 @@ pub fn part1<R: BufRead>(reader: R) -> u32 {
     counts.iter().sum()
 }
 
-pub fn part2<R: BufRead>(reader: R) -> BigInt {
+pub fn part2<R: BufRead>(reader: R) -> u64 {
     let line = reader.lines().next().unwrap().unwrap();
-    let mut counts: [BigInt; 9] = Default::default();
+    let mut counts = [0_u64; 9];
     line.split(',')
         .map(|n| n.parse::<usize>().unwrap())
         .for_each(|n| {
@@ -35,12 +33,12 @@ pub fn part2<R: BufRead>(reader: R) -> BigInt {
         });
 
     for _ in 0..256 {
-        let mut new_counts: [BigInt; 9] = Default::default();
+        let mut new_counts = [0; 9];
         for i in 0..8 {
-            new_counts[i] += &counts[i + 1];
+            new_counts[i] += counts[i + 1];
         }
-        new_counts[6] += &counts[0];
-        new_counts[8] += &counts[0];
+        new_counts[6] += counts[0];
+        new_counts[8] += counts[0];
 
         std::mem::swap(&mut new_counts, &mut counts);
     }
